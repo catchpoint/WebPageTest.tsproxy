f6ac2c56c269c22a2cee7fd4d772155f4b698668
Patrick Meenan <pmeenan@chromium.org>
2016-05-24 12:57:21 -0400

    Working in straight passthrough mode

--- tsproxy.py
+++ tsproxy.py
@@ -15,7 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import asyncore
+import signal
 import socket
+import sys
 
 server = None
 in_pipe = None
@@ -40,19 +42,117 @@ class TSPipe():
       if self.direction == self.PIPE_IN:
         peer = 'client'
       if peer in connections[connection_id]:
-        connections[connection_id][peer].handle_message(message)
+        try:
+          connections[connection_id][peer].handle_message(message)
+        except:
+          try:
+            connections[connection_id]['server'].close()
+          except:
+            pass
+          try:
+            connections[connection_id]['client'].close()
+          except:
+            pass
+          del connections[connection_id]
 
 
 ########################################################################################################################
 #   TCP Client
 ########################################################################################################################
-class TCPConnection(asyncore.dispatcher_with_send):
+class TCPConnection(asyncore.dispatcher):
+  STATE_ERROR = -1
+  STATE_IDLE = 0
+  STATE_RESOLVING = 1
+  STATE_RESOLVED = 2
+  STATE_CONNECTING = 3
+  STATE_CONNECTED = 4
+
   def __init__(self, client_id):
-    self.client_id = client_id
     asyncore.dispatcher.__init__(self)
+    self.client_id = client_id
+    self.state = self.STATE_IDLE
+    self.buffer = '';
+    self.addr = None
+
+  def SendMessage(self, type, message):
+    message['message'] = type
+    message['connection'] = self.client_id
+    in_pipe.SendMessage(message)
 
   def handle_message(self, message):
-    print '{0:d} TCPConnection processing message {1}'.format(self.client_id, message['message'])
+    if message['message'] == 'resolve':
+      self.HandleResolve(message)
+    if message['message'] == 'connect':
+      self.HandleConnect(message)
+    if message['message'] == 'data' and 'data' in message and len(message['data']) and self.state == self.STATE_CONNECTED:
+      self.buffer += message['data']
+    if message['message'] == 'closed':
+      if self.state != self.STATE_ERROR:
+        self.state = self.STATE_ERROR
+        if self.connected:
+          self.close()
+
+  def handle_error(self):
+    print '[{0:d}] Error'.format(self.client_id)
+    if self.state == self.STATE_CONNECTING:
+      self.SendMessage('connected', {'success': False, 'address': self.addr})
+    if self.state != self.STATE_ERROR:
+      self.state = self.STATE_ERROR
+      self.SendMessage('closed', {})
+    self.handle_error()
+
+  def handle_close(self):
+    self.close()
+    print '[{0:d}] Closed'.format(self.client_id)
+    if self.state == self.STATE_CONNECTING:
+      self.SendMessage('connected', {'success': False, 'address': self.addr})
+    if self.state != self.STATE_ERROR:
+      self.state = self.STATE_ERROR
+      self.SendMessage('closed', {})
+
+  def writable(self):
+    if self.state == self.STATE_CONNECTING:
+      self.state = self.STATE_CONNECTED
+      self.SendMessage('connected', {'success': True, 'address': self.addr})
+      print '[{0:d}] Connected'.format(self.client_id)
+    return (len(self.buffer) > 0)
+
+  def handle_write(self):
+    self.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    sent = self.send(self.buffer)
+    #print '[{0:d}] TCP => {1:d} byte(s)'.format(self.client_id, sent)
+    self.buffer = self.buffer[sent:]
+
+  def handle_read(self):
+    data = self.recv(1460)
+    if data:
+      #print '[{0:d}] TCP <= {1:d} byte(s)'.format(self.client_id, len(data))
+      self.SendMessage('data', {'data': data})
+
+  def HandleResolve(self, message):
+    #TODO (pmeenan): Run the actual lookup in a thread asynchronously
+    if 'hostname' in message:
+      hostname = message['hostname']
+    port = 0
+    if 'port' in message:
+      port = message['port']
+    print '[{0:d}] Resolving {1}:{2:d}'.format(self.client_id, hostname, port)
+    self.state = self.STATE_RESOLVING
+    try:
+      addresses = socket.getaddrinfo(hostname, port)
+    except:
+      addresses = ()
+      print '[{0:d}] Resolving {1}:{2:d} FAILED'.format(self.client_id, hostname, port)
+    self.state = self.STATE_RESOLVED
+    self.SendMessage('resolved', {'addresses': addresses})
+
+  def HandleConnect(self, message):
+    if 'addresses' in message and len(message['addresses']):
+      print '[{0:d}] Connecting'.format(self.client_id)
+      self.state = self.STATE_CONNECTING
+      self.addr = message['addresses'][0]
+      self.create_socket(self.addr[0], socket.SOCK_STREAM)
+      self.connect(self.addr[4])
 
 
 ########################################################################################################################
@@ -77,8 +177,8 @@ class Socks5Server(asyncore.dispatcher):
     pair = self.accept()
     if pair is not None:
       sock, addr = pair
-      print 'Incoming connection from %s' % repr(addr)
       self.current_client_id += 1
+      print '[{0:d}] Incoming connection from {1}'.format(self.current_client_id, repr(addr))
       connections[self.current_client_id] = {
         'client' : Socks5Connection(sock, self.current_client_id),
         'server' : None
@@ -86,7 +186,7 @@ class Socks5Server(asyncore.dispatcher):
 
 
 # Socks5 reference: https://en.wikipedia.org/wiki/SOCKS#SOCKS5
-class Socks5Connection(asyncore.dispatcher_with_send):
+class Socks5Connection(asyncore.dispatcher):
   STATE_ERROR = -1
   STATE_WAITING_FOR_HANDSHAKE = 0
   STATE_WAITING_FOR_CONNECT_REQUEST = 1
@@ -94,22 +194,50 @@ class Socks5Connection(asyncore.dispatcher_with_send):
   STATE_CONNECTING = 3
   STATE_CONNECTED = 4
 
-  def __init__(self, socket, client_id):
-    asyncore.dispatcher_with_send.__init__(self, socket)
+  def __init__(self, connected_socket, client_id):
+    asyncore.dispatcher.__init__(self, connected_socket)
     self.client_id = client_id
     self.state = self.STATE_WAITING_FOR_HANDSHAKE
     self.ip = None
+    self.addresses = None
     self.hostname = None
     self.port = None
+    self.requested_address = None
+    self.buffer = ''
+    self.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+  def SendMessage(self, type, message):
+    message['message'] = type
+    message['connection'] = self.client_id
+    out_pipe.SendMessage(message)
+
+  def handle_message(self, message):
+    if message['message'] == 'resolved':
+      self.HandleResolved(message)
+    if message['message'] == 'connected':
+      self.HandleConnected(message)
+    if message['message'] == 'data' and 'data' in message and len(message['data']) and self.state == self.STATE_CONNECTED:
+      self.send(message['data'])
+    if message['message'] == 'closed':
+      if self.state != self.STATE_ERROR:
+        self.state = self.STATE_ERROR
+        if self.connected:
+          self.close()
+
+  def writable(self):
+    return (len(self.buffer) > 0)
+
+  def handle_write(self):
+    sent = self.send(self.buffer)
+    self.buffer = self.buffer[sent:]
 
   def handle_read(self):
     global connections
     data = self.recv(1460) #Consume in packet-sized chunks (max)
     data_len = len(data)
     if data_len:
-      print '{0:d}: received {1:d} bytes'.format(self.client_id, len(data))
       if self.state == self.STATE_CONNECTED:
-        print 'Connected'
+        self.SendMessage('data', {'data': data})
       elif self.state == self.STATE_WAITING_FOR_HANDSHAKE:
         self.state = self.STATE_ERROR #default to an error state, set correctly if things work out
         if data_len >= 2 and ord(data[0]) == 0x05:
@@ -122,26 +250,25 @@ class Socks5Connection(asyncore.dispatcher_with_send):
                 supports_no_auth = True
           if supports_no_auth:
             # Respond with a message that "No Authentication" was agreed to
-            print '{0:d}: Responding to valid handhake request'.format(self.client_id)
+            print '[{0:d}] New Socks5 client'.format(self.client_id)
             response = chr(0x05) + chr(0x00)
             self.state = self.STATE_WAITING_FOR_CONNECT_REQUEST
-            self.send(response)
+            self.buffer += response
       elif self.state == self.STATE_WAITING_FOR_CONNECT_REQUEST:
         self.state = self.STATE_ERROR #default to an error state, set correctly if things work out
         if data_len >= 10 and ord(data[0]) == 0x05 and ord(data[2]) == 0x00:
           if ord(data[1]) == 0x01: #TCP connection (only supported method for now)
             connections[self.client_id]['server'] = TCPConnection(self.client_id)
+          self.requested_address = data[3:]
           port_offset = 0
           if ord(data[3]) == 0x01:
             port_offset = 8
             self.ip = '{0:d}.{1:d}.{2:d}.{3:d}'.format(ord(data[4]), ord(data[5]), ord(data[6]), ord(data[7]))
-            print '{0:d}: Connect to IPv4 address: {1}'.format(self.client_id, self.ip)
           elif ord(data[3]) == 0x03:
             name_len = ord(data[4])
             if data_len >= 6 + name_len:
               port_offset = 5 + name_len
               self.hostname = data[5:5 + name_len]
-              print '{0:d}: Connect to host: {1}'.format(self.client_id, self.hostname)
           elif ord(data[3]) == 0x04 and data_len >= 22:
             port_offset = 20
             self.ip = '';
@@ -149,25 +276,41 @@ class Socks5Connection(asyncore.dispatcher_with_send):
               self.ip += '{0:02x}'.format(ord(data[4 + i]))
               if i % 2 and i < 15:
                 self.ip += ':'
-            print '{0:d}: Connect to IPv6 address: {1}'.format(self.client_id, self.ip)
           if port_offset and connections[self.client_id]['server'] is not None:
             self.port = 256 * ord(data[port_offset]) + ord(data[port_offset + 1])
-            print '{0:d}: Port: {1:d}'.format(self.client_id, self.port)
             if self.port:
               if self.ip is None and self.hostname is not None:
                 self.state = self.STATE_RESOLVING
-                self.SendMessage('resolve', {'hostname': self.hostname})
+                self.SendMessage('resolve', {'hostname': self.hostname, 'port': self.port})
               elif self.ip is not None:
                 self.state = self.STATE_CONNECTING
-                self.SendMessage('connect', {'hostname': self.hostname})
+                self.addresses = socket.getaddrinfo(self.ip, self.port)
+                self.SendMessage('connect', {'addresses': self.addresses, 'port': self.port})
 
-  def SendMessage(self, type, message):
-    message['message'] = type
-    message['connection'] = self.client_id
-    out_pipe.SendMessage(message)
+  def handle_close(self):
+    self.close()
+    if self.state != self.STATE_ERROR:
+      self.state = self.STATE_ERROR
+      self.SendMessage('closed', {})
 
-  def handle_message(self, message):
-    print '{0:d} Socks5Connection processing message {1}'.format(self.client_id, message['message'])
+  def HandleResolved(self, message):
+    if 'addresses' in message:
+      self.state = self.STATE_CONNECTING
+      self.addresses = message['addresses']
+      self.SendMessage('connect', {'addresses': self.addresses, 'port': self.port})
+
+  def HandleConnected(self, message):
+    if 'success' in message and self.state == self.STATE_CONNECTING:
+      response = chr(0x05)
+      if message['success']:
+        response += chr(0x00)
+        self.state = self.STATE_CONNECTED
+      else:
+        response += chr(0x01)
+        self.state = self.STATE_ERROR
+      response += chr(0x00)
+      response += self.requested_address
+      self.buffer += response
 
 
 ########################################################################################################################
@@ -183,8 +326,15 @@ def main():
   options = parser.parse_args()
 
   print 'Starting Socks5 proxy server on {0}:{1:d}'.format(options.interface, options.port)
+  signal.signal(signal.SIGINT, signal_handler)
   server = Socks5Server(options.interface, options.port)
-  asyncore.loop()
+  asyncore.loop(timeout = 0.001, use_poll = True)
+
+def signal_handler(signal, frame):
+  global server
+  print('Exiting...')
+  del server
+  sys.exit(0)
 
 if '__main__' == __name__:
   main()
