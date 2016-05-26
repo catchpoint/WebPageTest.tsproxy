ea9b3431058fcc4c7e8df79217abb5b65d7c5221
Patrick Meenan <pmeenan@chromium.org>
2016-05-26 09:08:28 -0400

    Added logic to detect when the port is already in use

--- tsproxy.py
+++ tsproxy.py
@@ -282,10 +282,14 @@ class Socks5Server(asyncore.dispatcher):
   def __init__(self, host, port):
     asyncore.dispatcher.__init__(self)
     self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.set_reuse_addr()
-    self.bind((host, port))
-    self.listen(5)
-    self.current_client_id = 0
+    try:
+      #self.set_reuse_addr()
+      self.bind((host, port))
+      self.listen(5)
+      self.current_client_id = 0
+    except:
+      logging.critical("Unable to listen on {0}:{1}. Is the port already in use?".format(host, port))
+      exit(1)
 
   def handle_accept(self):
     global connections
